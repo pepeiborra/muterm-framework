235071321450fb58c27b424d71356794582fb4d8
Pepe Iborra <pepeiborra@gmail.com>
2009-08-22 11:01:16 +0200

    make the signature of mkDPProblem more informative and add some documentation

--- MuTerm/Framework/Problem.hs
+++ MuTerm/Framework/Problem.hs
@@ -36,11 +36,27 @@ import Text.XHtml (HTML(..))
 
 
 -----------------------------------------------------------------------------
-
 class IsDPProblem typ where
+{-
+-- Problems are modeled as a data family associated to the class IsDPProblem.
+-- The motivation to use a type class is to keep the type of problems open.
+-- The use of a data family to enclose the problem type is a win overall.
+   * Benefits:
+       - Separates the datatype of 'problem type' and the datatype carrying
+         the tuple of problem components. This allows for very natural definitions,
+         see e.g. Icap
+       - Keeps the 'problem type' and the tuple of components semantically connected.
+       - Allows us to define instances for DPProblems in general. E.g.
+            > instance (IsDPProblem typ, HasRules t v trs) => HasRules t v (DPProblem typ trs) where
+         as opposed to
+            > instance (IsDPProblem p, HasRules t v trs) => HasRules t v (p trs) where
+         which is more opaque to the GHC type checker
+-}
+
+
     data DPProblem typ :: * -> *
     getProblemType :: DPProblem typ trs -> typ
-    mkDPProblem    :: typ -> trs -> trs -> DPProblem typ trs
+    mkDPProblem    :: (rules ~ trs, pairs ~ trs) => typ -> rules -> pairs -> DPProblem typ trs
     getP, getR     :: DPProblem typ trs -> trs
     mapP, mapR     :: (trs -> trs) -> DPProblem typ trs -> DPProblem typ trs
 

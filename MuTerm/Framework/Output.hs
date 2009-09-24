85c24ef943f131285d33ede9c5773a4bd3b3e552
Pepe Iborra <pepeiborra@gmail.com>
2009-09-24 14:57:23 +0200

    Revert "Follow an API change in the graphviz package"
    Go back to the old graphviz version
    
    This reverts commit e318a77a1e6d681b26423000dc6e51bb17a8159e.

--- MuTerm/Framework/Output.hs
+++ MuTerm/Framework/Output.hs
@@ -138,7 +138,7 @@ divmaybe  = divresult +++ spani "maybe" << "Fail. "
 instance (IsDPProblem typ, Pretty rules) => DotRep (DPProblem typ [rules]) where
   dot p = Text rep atts where
     atts = [ Shape BoxShape
-           , Style [SItem Bold []]
+           , Style (Stl Bold Nothing)
            , FontName "monospace"
            , FontSize 10
            , Margin (PVal (PointD 0.2 0.2))]

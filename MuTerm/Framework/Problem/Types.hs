e28fe77aaa8a0b21314da293198ece54884d5aee
Pepe Iborra <pepeiborra@gmail.com>
2009-09-29 18:50:56 +0200

    Add some standard problem types

---
+++ MuTerm/Framework/Problem/Types.hs
@@ -0,0 +1,19 @@
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  MuTerm.Framework.Proof
+-- Copyright   :  (c) muterm development team
+-- License     :  see LICENSE
+--
+-- Maintainer  :  rgutierrez@dsic.upv.es
+-- Stability   :  unstable
+-- Portability :  non-portable
+--
+-- This module contains standard problem types
+--
+-----------------------------------------------------------------------------
+
+module MuTerm.Framework.Problem.Types where
+
+data Rewriting  = Rewriting  deriving (Eq, Ord, Show)
+data IRewriting = IRewriting deriving (Eq, Ord, Show)
